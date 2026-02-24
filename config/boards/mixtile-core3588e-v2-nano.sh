ced834ae1fd51ad4b11efe602fe806c9bd7704e9
jmq <296806834@qq.com>
2026-02-24 14:13:52 +0800

    add core3588ev2, delete ubuntu22

---
+++ config/boards/mixtile-core3588e-v2-nano.sh
@@ -0,0 +1,101 @@
+# shellcheck shell=bash
+
+export BOARD_NAME="Mixtile Core 3588E"
+export BOARD_MAKER="Mixtile"
+export BOARD_SOC="Rockchip RK3588"
+export BOARD_CPU="ARM Cortex A76 / A55"
+export KERNEL_PACKAGE="kernel-mixtile-rk3588-core3588e-v2-nano"
+export UBOOT_PACKAGE="u-boot-mixtile-rk3588-core3588e-v2-nano"
+export UBOOT_RULES_TARGET="mixtile-core3588e-v2-nano-rk3588"
+export COMPATIBLE_SUITES=("jammy" "noble")
+export COMPATIBLE_FLAVORS=("server" "desktop")
+
+function config_image_hook__mixtile-core3588e-v2-nano() {
+    local rootfs="$1"
+    local suite="$3"
+
+    if [ "${suite}" == "jammy" ]; then
+        # Install panfork
+        chroot "${rootfs}" add-apt-repository -y ppa:jjriek/panfork-mesa
+        chroot "${rootfs}" apt-get update
+        chroot "${rootfs}" apt-get -y install mali-g610-firmware
+        chroot "${rootfs}" apt-get -y dist-upgrade
+
+        # Install libmali blobs alongside panfork
+        chroot "${rootfs}" apt-get -y install libmali-g610-x11
+
+        # Install the rockchip camera engine
+        chroot "${rootfs}" apt-get -y install camera-engine-rkaiq-rk3588
+    fi
+
+     if [ "${suite}" == "noble" ]; then
+	# Install libmali blobs
+	cp -r ../packages/libmali/libmali-valhall-g610-g24p0-x11-wayland-gbm_1.9-1_arm64.deb ${rootfs}/tmp
+	chroot "${rootfs}" apt install -y /tmp/libmali-valhall-g610-g24p0-x11-wayland-gbm_1.9-1_arm64.deb
+	
+	# Install the rockchip camera engine
+	chroot "${rootfs}" apt-get -y install camera-engine-rkaiq-rk3588
+    fi
+
+    cp ${overlay_dir}/usr/bin/vendor_storage ${rootfs}/usr/bin/vendor_storage
+    cp -r ../packages/adb/rockchip-adbd.deb ${rootfs}/tmp
+    chroot "${rootfs}" dpkg -i /tmp/rockchip-adbd.deb
+    echo "BUILD_ID=$(date +'%Y-%m-%d')" >> "${rootfs}/etc/os-release"
+
+    chroot "${rootfs}" sed -i '/^menu title/d' /usr/sbin/u-boot-update
+    chroot "${rootfs}" sed -i 's/^U_BOOT_PROMPT=.*$/U_BOOT_PROMPT="0"/' /usr/share/u-boot-menu/conf.d/ubuntu.conf
+    chroot "${rootfs}" apt-mark hold u-boot-menu
+
+    if [ "$FLAVOR" = "server" ]; then
+        chroot "${rootfs}" apt-get -y purge cloud-init
+        cp ${overlay_dir}/boot/firmware/network-config ${rootfs}/etc/netplan/default.yaml
+    fi
+
+    cat << EOF | chroot "${rootfs}"
+export DEBIAN_FRONTEND=noninteractive
+export LANG=en_US.UTF-8
+
+/usr/sbin/oem-config-remove
+userdel --force --remove oem || true
+systemctl set-default graphical.target || true
+systemctl disable oem-config.service || true
+systemctl disable oem-config.target || true
+rm -f /lib/systemd/system/oem-config.* || true
+
+sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/default/locale
+dpkg-reconfigure locales
+
+useradd mixtile -m -u 1000 -s /bin/bash -G sudo,netdev,audio,video,disk,tty,users,games,dialout,plugdev,input,bluetooth,systemd-journal,render
+(
+echo "mixtile"
+echo "mixtile"
+) | passwd "root" > /dev/null 2>&1
+
+(
+echo "mixtile"
+echo "mixtile"
+) | passwd "mixtile" > /dev/null 2>&1
+
+
+sed -i -r \
+    -e "s/^#[ ]*AutomaticLoginEnable =.*\$/AutomaticLoginEnable=true/" \
+    -e "s/^#[ ]*AutomaticLogin =.*\$/AutomaticLogin=mixtile/" \
+    /etc/gdm3/custom.conf
+
+ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime
+echo "Etc/UTC" >/etc/timezone
+echo "mixtile-ubuntu" >/etc/hostname
+
+echo "127.0.0.1   localhost
+127.0.1.1   mixtile-ubuntu
+::1         localhost mixtile-ubuntu ip6-localhost ip6-loopback
+fe00::0     ip6-localnet
+ff00::0     ip6-mcastprefix
+ff02::1     ip6-allnodes
+ff02::2     ip6-allrouters" > /etc/hosts
+
+EOF
+
+    return 0
+}
---
+++ packages/u-boot-mixtile-rk3588-core3588e-v2-nano/debian/targets.mk
@@ -0,0 +1,146 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += rock-5b-rk3588
+rock-5b-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5b-rk3588_bl31 := rk3588_bl31_v1.45.elf
+rock-5b-rk3588_pkg := rock-5b
+
+u-boot-rockchip_platforms += rock-5b-plus-rk3588
+rock-5b-plus-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5b-plus-rk3588_bl31 := rk3588_bl31_v1.45.elf
+rock-5b-plus-rk3588_pkg := rock-5b-plus
+
+u-boot-rockchip_platforms += rock-5a-rk3588s
+rock-5a-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5a-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+rock-5a-rk3588s_pkg := rock-5a
+
+u-boot-rockchip_platforms += rock-5a-spi-rk3588s
+rock-5a-spi-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5a-spi-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+rock-5a-spi-rk3588s_pkg := rock-5a-spi
+
+u-boot-rockchip_platforms += rock-5d-rk3588s
+rock-5d-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5d-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+rock-5d-rk3588s_pkg := rock-5d
+
+u-boot-rockchip_platforms += rock-5c-rk3588s
+rock-5c-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5c-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+rock-5c-rk3588s_pkg := rock-5c
+
+u-boot-rockchip_platforms += rock-5-itx-rk3588
+rock-5-itx-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5-itx-rk3588_bl31 := rk3588_bl31_v1.45.elf
+rock-5-itx-rk3588_pkg := rock-5-itx
+
+u-boot-rockchip_platforms += radxa-cm5-io-rk3588s
+radxa-cm5-io-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+radxa-cm5-io-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+radxa-cm5-io-rk3588s_pkg := radxa-cm5-io
+
+u-boot-rockchip_platforms += radxa-cm5-rpi-cm4-io-rk3588s
+radxa-cm5-rpi-cm4-io-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+radxa-cm5-rpi-cm4-io-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+radxa-cm5-rpi-cm4-io-rk3588s_pkg := radxa-cm5-rpi-cm4-io
+
+u-boot-rockchip_platforms += radxa-nx5-io-rk3588s
+radxa-nx5-io-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+radxa-nx5-io-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+radxa-nx5-io-rk3588s_pkg := radxa-nx5-io
+
+u-boot-rockchip_platforms += nanopi-r6s-rk3588s
+nanopi-r6s-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+nanopi-r6s-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+nanopi-r6s-rk3588s_pkg := nanopi-r6s
+
+u-boot-rockchip_platforms += nanopi-r6c-rk3588s
+nanopi-r6c-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+nanopi-r6c-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+nanopi-r6c-rk3588s_pkg := nanopi-r6c
+
+u-boot-rockchip_platforms += nanopc-t6-rk3588
+nanopc-t6-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+nanopc-t6-rk3588_bl31 := rk3588_bl31_v1.45.elf
+nanopc-t6-rk3588_pkg := nanopc-t6
+
+u-boot-rockchip_platforms += lubancat-4-rk3588s
+lubancat-4-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+lubancat-4-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+lubancat-4-rk3588s_pkg := lubancat-4
+
+u-boot-rockchip_platforms += indiedroid-nova-rk3588s
+indiedroid-nova-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+indiedroid-nova-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+indiedroid-nova-rk3588s_pkg := indiedroid-nova
+
+u-boot-rockchip_platforms += armsom-w3-rk3588
+armsom-w3-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+armsom-w3-rk3588_bl31 := rk3588_bl31_v1.45.elf
+armsom-w3-rk3588_pkg := armsom-w3
+
+u-boot-rockchip_platforms += armsom-sige7-rk3588
+armsom-sige7-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+armsom-sige7-rk3588_bl31 := rk3588_bl31_v1.45.elf
+armsom-sige7-rk3588_pkg := armsom-sige7
+
+u-boot-rockchip_platforms += armsom-aim7-rk3588
+armsom-aim7-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+armsom-aim7-rk3588_bl31 := rk3588_bl31_v1.45.elf
+armsom-aim7-rk3588_pkg := armsom-aim7
+
+u-boot-rockchip_platforms += roc-rk3588s-pc-rk3588s
+roc-rk3588s-pc-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+roc-rk3588s-pc-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+roc-rk3588s-pc-rk3588s_pkg := roc-rk3588s-pc
+
+u-boot-rockchip_platforms += aio-3588l-rk3588
+aio-3588l-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+aio-3588l-rk3588_bl31 := rk3588_bl31_v1.45.elf
+aio-3588l-rk3588_pkg := aio-3588l
+
+u-boot-rockchip_platforms += mixtile-core3588e-rk3588
+mixtile-core3588e-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+mixtile-core3588e-rk3588_bl31 := rk3588_bl31_v1.45.elf
+mixtile-core3588e-rk3588_pkg := mixtile-core3588e
+
+u-boot-rockchip_platforms += mixtile-core3588e-v2-nano-rk3588
+mixtile-core3588e-v2-nano-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+mixtile-core3588e-v2-nano-rk3588_bl31 := rk3588_bl31_v1.45.elf
+mixtile-core3588e-v2-nano-rk3588_pkg := mixtile-core3588e-v2-nano
+
+u-boot-rockchip_platforms += orangepi-cm5-rk3588s
+orangepi-cm5-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-cm5-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+orangepi-cm5-rk3588s_pkg := orangepi-cm5
+
+u-boot-rockchip_platforms += orangepi-5-rk3588s
+orangepi-5-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5-rk3588s_pkg := orangepi-5
+
+u-boot-rockchip_platforms += orangepi-5-sata-rk3588s
+orangepi-5-sata-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5-sata-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+
+u-boot-rockchip_platforms += orangepi-5b-rk3588s
+orangepi-5b-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5b-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5b-rk3588s_pkg := orangepi-5b
+
+u-boot-rockchip_platforms += orangepi-5-pro-rk3588s
+orangepi-5-pro-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5-pro-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5-pro-rk3588s_pkg := orangepi-5-pro
+
+u-boot-rockchip_platforms += orangepi-5-plus-rk3588
+orangepi-5-plus-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5-plus-rk3588_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5-plus-rk3588_pkg := orangepi-5-plus
+
+u-boot-rockchip_platforms += orangepi-5-max-rk3588
+orangepi-5-max-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5-max-rk3588_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5-max-rk3588_pkg := orangepi-5-max
